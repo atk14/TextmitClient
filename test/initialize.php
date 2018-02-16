713f7ee27f5f5f7437756c2591fbf1b818a6ed1f
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-02-16 14:30:25 +0100

    Textmit requires php>=5.3.0 and atk14/api-data-fetcher 1.*

--- test/initialize.php
+++ test/initialize.php
@@ -9,5 +9,7 @@ require( __DIR__ . "/../src/textmit.php" );
 require( __DIR__ . "/../src/textmit_result.php" );
 require( __DIR__ . "/../src/fulltext_data.php" );
 
+require(__DIR__ . "/../vendor/autoload.php");
+
 require( __DIR__ . "/page_component.php" );
 require( __DIR__ . "/api_data_fetcher.php" );
